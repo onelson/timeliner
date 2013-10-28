42563f49c54f8d68d327a20c28e60b33bfbf0109
Owen Nelson <onelson@gmail.com>
2013-10-27 20:26:09 -0700

    Merging project setup

---
+++ client-src/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('timelinerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
