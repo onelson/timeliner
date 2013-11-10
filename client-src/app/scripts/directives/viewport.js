4dec2cac8f8022fd9a2968149df888e03056c59f
Owen Nelson <onelson@gmail.com>
2013-11-10 14:25:00 -0800

    setting up a drag event so we can pan the content of the viewport

--- client-src/app/scripts/directives/viewport.js
+++ client-src/app/scripts/directives/viewport.js
@@ -1,12 +1,36 @@
+
 'use strict';
 
 angular.module('timeliner')
   .directive('viewport', function () {
     return {
-      template: '<div></div>',
+      scope: {},
+      templateUrl: 'views/viewport.html',
       restrict: 'E',
+      controller: ['$scope', function($scope) {
+
+        var isDragging = false;
+
+        $scope.press = function mousePress () {
+          isDragging = true;
+        };
+
+        $scope.release = function mouseRelease () {
+          isDragging = false;
+        };
+
+        /**
+         * when dragging, we want to adjust the coords shown in the viewport
+         */
+        $scope.checkMove = function checkMove () {
+          if (isDragging) {
+            console.log(window.event);
+          }
+        };
+
+      }],
       link: function postLink(scope, element, attrs) {
-        element.text('this is the viewport directive');
+
       }
     };
   });
